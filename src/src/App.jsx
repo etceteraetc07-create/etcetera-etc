a39baee00442021948fb9fefa6df8d9125fb4682
etceteraetc07-create <etceteraetc07@gmail.com>
2025-09-30 17:16:27 +0800

    Delete src directory

--- src/src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ textAlign: 'center', padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Welcome to Etcetera Etc.</h1>
-      <p>Because one etcetera wasn’t enough 😉</p>
-      <p>Discover trending, clever, and everyday finds all in one witty shop.</p>
-    </div>
-  )
-}
