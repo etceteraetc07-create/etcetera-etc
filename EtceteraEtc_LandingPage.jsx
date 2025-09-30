d8aec108d6b44036dd788266890eb861a72bbdda
etceteraetc07-create <etceteraetc07@gmail.com>
2025-09-30 16:55:40 +0800

    Add files via upload

---
+++ EtceteraEtc_LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <section style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Etcetera Etc.</h1>
+      <p>Because one etcetera wasn’t enough.</p>
+      <button style={{ padding: '10px 20px', fontSize: '16px' }}>Shop Now</button>
+    </section>
+  )
+}
+
+export default LandingPage
---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from './components/EtceteraEtc_LandingPage.jsx'
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
